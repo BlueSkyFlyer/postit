4349b3971870f46fd889e2a6eb990ef067a6c175
Jack Collins <BlueSkyFlyer2@gmail.com>
2013-10-06 20:22:09 -0500

    Forms, Strong Params, Validations, and Partials

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,22 +8,37 @@ class PostsController < ApplicationController
   end
 
   def new
-
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
 
+    if @post.save
+      flash[:notice] = "Your post was saved."
+      redirect_to posts_path
+    else
+      render :new
+    end
   end
 
   def edit
-
+    @post = Post.find(params[:id])
   end
 
   def update
-
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+    	flash[:notice] = "This post was updated!"
+    	redirect_to post_path(@post)
+    else
+    	render :edit
+    end
   end
 
-  def destroy
-  	
+  private
+
+  def post_params
+  	params.require(:post).permit!
   end
 end
