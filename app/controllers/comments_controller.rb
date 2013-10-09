c5eb2234da5563c4f36d268c60d970a7e0571500
Jack Collins <BlueSkyFlyer2@gmail.com>
2013-10-08 21:21:42 -0500

    Got comments working

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,18 @@
 class CommentsController < ApplicationController
 	def create
+		@post = Post.find(params[:post_id])
+		@comment = @post.comments.new(comment_params)
+
+		if @comment.save
+			flash[:notice] = "Your comment was submitted"
+			redirect_to post_path(@post)
+		else
+			@post.reload
+			render "posts/show"
+		end
+	end
+
+	def comment_params
+		params.require(:comment).permit!
 	end
 end
