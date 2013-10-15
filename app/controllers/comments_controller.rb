14d0df59202122cc57264c6ed8f05cbf86a5b5d4
Jack Collins <BlueSkyFlyer2@gmail.com>
2013-10-14 20:08:43 -0500

    before action, helpers, and multi select dropdown

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,17 +2,16 @@ class CommentsController < ApplicationController
 	def create
 		@post = Post.find(params[:post_id])
 		@comment = @post.comments.new(comment_params)
+		@comment.user_id = 1 # Will improve this soon
 
 		if @comment.save
-			flash[:notice] = "Your comment was submitted"
-			redirect_to post_path(@post)
+			redirect_to post_path(@post), notice: "Your comment was submitted"
 		else
-			@post.reload
 			render "posts/show"
 		end
 	end
 
 	def comment_params
-		params.require(:comment).permit!
+		params.require(:comment).permit(:body)
 	end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,20 +1,22 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update]
+  
+
   def index
     @posts = Post.all
   end
 
   def show
-    @post = Post.find(params[:id])
     @comment = Comment.new
   end
 
   def new
     @post = Post.new
+    @categories = Category.all
   end
 
   def create
     @post = Post.new(post_params)
-
     if @post.save
       flash[:notice] = "Your post was saved."
       redirect_to posts_path
@@ -24,11 +26,9 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
     if @post.update(post_params)
     	flash[:notice] = "This post was updated!"
     	redirect_to post_path(@post)
@@ -41,5 +41,9 @@ class PostsController < ApplicationController
 
   def post_params
   	params.require(:post).permit!
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
